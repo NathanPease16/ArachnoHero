9759fa3ec576e1d094b8b494637afd49cbc35311
MagiGuy <jdjamrosz@stu.naperville203.org>
2023-06-09 02:25:24 -0500

    Update Movement.cs

--- ArachnoHero/Assets/Scripts/Movement.cs
+++ ArachnoHero/Assets/Scripts/Movement.cs
@@ -11,6 +11,7 @@ public class Movement : MonoBehaviour
     [Header("Moving")]
     [SerializeField] private float moveSpeed;
     [SerializeField] private float maxSpeed;
+    [SerializeField] private float dampenFactor;
 
     [Header("Jumping")]
     [SerializeField] private float jumpForce;
@@ -27,16 +28,28 @@ public class Movement : MonoBehaviour
     void Update()
     {
         Move();
+        Jump();
     }
 
     private void Move()
     {
-        Vector3 move = (transform.forward * Input.GetAxis("Vertical") + transform.right * Input.GetAxis("Horizontal")).normalized;
+        Vector3 move = (transform.forward * Input.GetAxisRaw("Vertical") + transform.right * Input.GetAxisRaw("Horizontal")).normalized;
         Vector3 currentVelocity = new Vector3(rigidbody.velocity.x, 0f, rigidbody.velocity.z);
         Vector3 idealVelocity = move * maxSpeed;
         Vector3 deltaVelocity = idealVelocity - currentVelocity;
-        if(deltaVelocity.magnitude > moveSpeed) {deltaVelocity = Vector3.ClampMagnitude(deltaVelocity, moveSpeed);}
+        if(deltaVelocity.magnitude > moveSpeed) {
+            deltaVelocity = Vector3.ClampMagnitude(deltaVelocity, moveSpeed);
+        }
+        Debug.Log($"Current Velocity: {currentVelocity.magnitude}    Ideal Velocity: {idealVelocity.magnitude}");
         rigidbody.AddForce(deltaVelocity);
+    }
+
+    private void Jump()
+    {
+        if(Input.GetKeyDown(KeyCode.Space) && IsGrounded()) {
+            Debug.Log("aaa");
+            rigidbody.AddForce(Vector3.up*jumpForce, ForceMode.Impulse);
+        }
     }
 
     private bool IsGrounded()
