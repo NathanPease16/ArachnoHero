3343e3e0a85b1b3d64b04edf32e3c63faaa01a8c
MagiGuy <jdjamrosz@stu.naperville203.org>
2023-06-09 16:30:42 -0500

    cool ui

--- ArachnoHero/Assets/Scripts/UI/CrosshairUI.cs
+++ ArachnoHero/Assets/Scripts/UI/CrosshairUI.cs
@@ -6,14 +6,26 @@ public class CrosshairUI : MonoBehaviour
 {
     [Header("References")]
     private Grappling grapple;
+    private Animation anim;
+
+    // Variables
+    private bool isLocked;
 
     void Awake()
     {
         grapple = GameObject.Find("Player").GetComponent<Grappling>();
+        anim = GetComponent<Animation>();
+        isLocked = false;
     }
 
     void Update()
     {
-
+        bool wasLocked = isLocked;
+        isLocked = grapple.CanGrapple();
+        if(wasLocked != isLocked) {
+            Debug.Log("aaa");
+            if(isLocked) {anim.Play("CrosshairLock");}
+            else {anim.Play("CrosshairUnlock");}
+        }
     }
 }
