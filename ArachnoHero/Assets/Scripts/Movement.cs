9ded956979602e172ab2e27a5ad9ead3fab76765
MagiGuy <jdjamrosz@stu.naperville203.org>
2023-06-09 01:57:25 -0500

    Movement :D
    physic

--- ArachnoHero/Assets/Scripts/Movement.cs
+++ ArachnoHero/Assets/Scripts/Movement.cs
@@ -4,6 +4,7 @@ public class Movement : MonoBehaviour
 {
     // References
     private new Rigidbody rigidbody;
+    private Transform mainCamera;
     private Transform groundCheck;
 
     // Attributes
@@ -19,6 +20,7 @@ public class Movement : MonoBehaviour
     void Awake()
     {
         rigidbody = GetComponent<Rigidbody>();
+        mainCamera = Camera.main.transform;
         groundCheck = transform.Find("Ground Check");
     }
 
@@ -29,28 +31,12 @@ public class Movement : MonoBehaviour
 
     private void Move()
     {
-        float forward = 0;
-        float right = 0;
-        float up = rigidbody.velocity.y;
-
-        if (Input.GetKey(KeyCode.W))
-            forward += moveSpeed;
-            
-        if (Input.GetKey(KeyCode.S))
-            forward -= moveSpeed;
-
-        if (Input.GetKey(KeyCode.A))
-            right -= moveSpeed;
-
-        if (Input.GetKey(KeyCode.D))
-            right += moveSpeed;
-
-        if (Input.GetKey(KeyCode.Space) && IsGrounded())
-            up = jumpForce;
-
-        Vector3 moveDirection = transform.forward * forward + transform.right * right;
-        moveDirection = new Vector3(moveDirection.x, up, moveDirection.z);
-        rigidbody.velocity = moveDirection;
+        Vector3 move = (transform.forward * Input.GetAxis("Vertical") + transform.right * Input.GetAxis("Horizontal")).normalized;
+        Vector3 currentVelocity = rigidbody.velocity;
+        Vector3 idealVelocity = move * maxSpeed;
+        Vector3 deltaVelocity = idealVelocity - currentVelocity;
+        if(deltaVelocity.magnitude > moveSpeed) {deltaVelocity = Vector3.ClampMagnitude(deltaVelocity, moveSpeed);}
+        rigidbody.AddForce(deltaVelocity);
     }
 
     private bool IsGrounded()
